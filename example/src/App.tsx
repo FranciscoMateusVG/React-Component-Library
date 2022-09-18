f088d9f3535d8f1eb637e505bf2ff9d2d75f055c
FranciscoMateusVG <49985463+FranciscoMateusVG@users.noreply.github.com>
2022-09-18 16:58:47 -0300

    Published button

---
+++ example/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@franciscomateusvg/react-component-library'
+
+function App() {
+  return (
+    <div className='App'>
+      <Button label='Try me!' />
+    </div>
+  )
+}
+
+export default App
